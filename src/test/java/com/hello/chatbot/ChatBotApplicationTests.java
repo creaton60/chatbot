f8f82ef91431141ecd8c258ee78b1be97c1b2c93
dklee <lovmistake@gmail.com>
2018-03-23 21:37:01 +0900

    [create] project init

---
+++ src/test/java/com/hello/chatbot/ChatBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hello.chatbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
