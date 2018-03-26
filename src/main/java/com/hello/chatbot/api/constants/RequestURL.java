e07c6bf67fe8a43d2ac0b4e7e5d98684024e267c
reactor <lovmistake@gmail.com>
2018-03-27 07:34:04 +0900

    [update feature] related add api url

--- src/main/java/com/hello/chatbot/api/constants/RequestURL.java
+++ src/main/java/com/hello/chatbot/api/constants/RequestURL.java
@@ -2,4 +2,9 @@ package com.hello.chatbot.api.constants;
 
 public class RequestURL {
     public static final String CHATBOT_INDEX="/";
+
+    //Buidler
+    public static final String CHATBOT_BOT_CREATE="/api/{version}/bots"; //post
+    public static final String CHATBOT_BOT_MODIFY="/api/{version}/bots"; //put
+
 }
---
+++ src/main/java/com/hello/chatbot/common/api/builder/ApiBuilderCreateMessage.java
@@ -0,0 +1,9 @@
+package com.hello.chatbot.common.api.builder;
+
+import com.hello.chatbot.common.api.ChatBotApiMessage;
+
+
+public class ApiBuilderCreateMessage extends ChatBotApiMessage{
+
+
+}
