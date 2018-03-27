ce9209d1f3d68f4197edbd78fb3c71f0013d419c
reactor <lovmistake@gmail.com>
2018-03-27 22:56:57 +0900

    [update feature] update api request url

--- src/main/java/com/hello/chatbot/api/constants/RequestURL.java
+++ src/main/java/com/hello/chatbot/api/constants/RequestURL.java
@@ -3,8 +3,26 @@ package com.hello.chatbot.api.constants;
 public class RequestURL {
     public static final String CHATBOT_INDEX="/";
 
-    //Buidler
+    //Bot Generate
     public static final String CHATBOT_BOT_CREATE="/api/{version}/bots"; //post
     public static final String CHATBOT_BOT_MODIFY="/api/{version}/bots"; //put
+    public static final String CHATBOT_BOT_INFO="/api/{version}/bots/{botId}"; //get
+    public static final String CHATBOT_BOT_LIST="/apo/{version}/bots"; //get
+    public static final String CHATBOT_BOT_WITHDRAW="/api/{version}/bots/{botId}"; //delete
+
+    //Event Generate
+    public static final String CHATBOT_EVENT_CREATE="/api/{version}/events"; //post
+
+    //Action Generate
+    public static final String CHATBOT_ACTION_CREATE="/api/{version}/actions"; //post
+    public static final String CHATBOT_ACTION_MODIFY="/api/{version}/actions"; //put
+
+    //Card Generate
+    public static final String CHATBOT_CARD_CREATE="/api/{version}/cards"; //post
+    public static final String CHATBOT_CARD_LIST="/api/{version}/cards/{cardId}"; //get
+    public static final String CHATBOT_CARD_INFO="/api/{version}/cards/{cardId}/info/{infoId}"; //get
+
+    //Chat Input
+    public static final String CHATBOT_CHAT_EVENT="/api/{version}/event/{botId}/{eventId}/"; //put
 
 }
