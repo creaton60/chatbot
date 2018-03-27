2345070ae31e2616cf732603f1422822c9c4d689
reactor <lovmistake@gmail.com>
2018-03-28 08:27:10 +0900

    [update feature] domain event process

--- src/main/java/com/hello/chatbot/api/controller/EventController.java
+++ src/main/java/com/hello/chatbot/api/controller/EventController.java
@@ -1,11 +1,42 @@
 package com.hello.chatbot.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hello.chatbot.api.service.EventService;
+import com.hello.chatbot.common.api.ChatBotApiMessage;
+import com.hello.chatbot.common.client.ChatBotClientMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 import static com.hello.chatbot.api.constants.RequestURL.*;
 
 @RestController
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping(produces = "application/json", value = CHATBOT_EVENT_CREATE)
+    public ChatBotApiMessage createEvent(@PathVariable int version,
+                                         @Valid @RequestBody ChatBotClientMessage message){
+
+        return eventService.createEvent(message);
+    }
+
+    @PutMapping(produces = "application/json", value = CHATBOT_EVENT_MODIFY)
+    public ChatBotApiMessage updateEvent(@PathVariable int version,
+                                         @Valid @RequestBody ChatBotClientMessage message){
+
+        return eventService.modifyEvent(message);
+    }
+
+    @GetMapping(produces = "application/json", value = CHATBOT_EVENT_MENU)
+    public ChatBotApiMessage getChatBotMenuList(@PathVariable int version,
+                                                @PathVariable int botId){
+
+        return eventService.getMenuList(botId);
+    }
+
 
 
 }
--- src/main/java/com/hello/chatbot/api/service/EventService.java
+++ src/main/java/com/hello/chatbot/api/service/EventService.java
@@ -1,4 +1,13 @@
 package com.hello.chatbot.api.service;
 
+import com.hello.chatbot.common.api.ChatBotApiMessage;
+import com.hello.chatbot.common.client.ChatBotClientMessage;
+
 public interface EventService {
+
+    ChatBotApiMessage createEvent(ChatBotClientMessage message);
+
+    ChatBotApiMessage modifyEvent(ChatBotClientMessage message);
+
+    ChatBotApiMessage getMenuList(int botId);
 }
